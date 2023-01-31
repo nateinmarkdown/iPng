8cf688a10fcc683b6e92c644d08cdf85338f939a
Nate <121343177+nateinmarkdown@users.noreply.github.com>
2023-01-31 17:09:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ $(document).ready(function() {
     event.preventDefault();
     let fromFormat = $("#from-format").val();
     let toFormat = $("#to-format").val();
-    alert(`Converting from ${fromFormat} to ${toFormat}...`);
+    let file = $("#file")[0].files[0];
+
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function() {
+      let image = new Image();
+      image.src = reader.result;
+      image.onload = function() {
+        let canvas = document.createElement("canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        let context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0, image.width, image.height);
+        let data = canvas.toDataURL("image/" + toFormat);
+        let a = document.createElement("a");
+        a.href = data;
+        a.download = file.name.split(".")[0] + "." + toFormat;
+        a.click();
+      };
+    };
   });
 });
